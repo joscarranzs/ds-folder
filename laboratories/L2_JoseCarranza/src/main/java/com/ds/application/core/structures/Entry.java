10fd983d8822882811b7221d919c469d665fc856
joscarranzs <jcarranzsosa@gmail.com>
2026-04-26 10:44:21 -0500

    Update Entry.java

--- laboratories/L2_JoseCarranza/src/main/java/com/ds/application/core/structures/Entry.java
+++ laboratories/L2_JoseCarranza/src/main/java/com/ds/application/core/structures/Entry.java
@@ -1,27 +1,27 @@
 package com.ds.application.core.structures;
 
 public class Entry<K, V> {
-    private K key;
-    private V value;
+  private K key;
+  private V value;
 
-    public Entry(K key, V value) {
-        this.key = key;
-        this.value = value;
-    }
+  public Entry(K key, V value) {
+    this.key = key;
+    this.value = value;
+  }
 
-    public K getKey() {
-        return key;
-    }
+  public K getKey() {
+    return key;
+  }
 
-    public V getValue() {
-        return value;
-    }
+  public V getValue() {
+    return value;
+  }
 
-    public void setKey(K key) {
-        this.key = key;
-    }
+  public void setKey(K key) {
+    this.key = key;
+  }
 
-    public void setValue(V value) {
-        this.value = value;
-    }
+  public void setValue(V value) {
+    this.value = value;
+  }
 }
