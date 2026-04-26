f53d69cc1e6506ef8f01765534b975b30c33ae29
Sam-Code0607 <franciscocode.dev@gmail.com>
2026-04-26 01:10:19 -0500

    All UI elements created

--- laboratories/L2_JoseCarranza/src/main/java/com/ds/application/view/components/elements/ui/ButtonElement.java
+++ laboratories/L2_JoseCarranza/src/main/java/com/ds/application/view/components/elements/ui/ButtonElement.java
@@ -1,7 +1,22 @@
 // Elemento de UI usado para construir componentes como header, sidebar y controles de formulario.
-
 package com.ds.application.view.components.elements.ui;
+
+import javafx.scene.control.Button;
 
 public class ButtonElement {
     // TODO: implement button component for JavaFX
+    private Button button;
+    public ButtonElement(String text){
+        button = new Button(text);
+        applyStyles();
+    }
+    private void applyStyles(){
+        //clase CSS
+        button.getStyleClass();
+    }
+    
+    public Button getNode(){
+        return button;
+    }
+
 }
--- laboratories/L2_JoseCarranza/src/main/java/com/ds/application/view/components/elements/ui/InputElement.java
+++ laboratories/L2_JoseCarranza/src/main/java/com/ds/application/view/components/elements/ui/InputElement.java
@@ -1,7 +1,25 @@
 // Elemento de UI usado para construir componentes como header, sidebar y controles de formulario.
-
 package com.ds.application.view.components.elements.ui;
 
+import javafx.scene.control.TextField;
 public class InputElement {
     // TODO: implement input component for JavaFX
+    private TextField input;
+    
+ public InputElement(String placeholder){
+    input = new TextField();
+    input.setPromptText(placeholder);
+    applyStyles();
+ }
+   private void applyStyles(){
+      //clase CSS
+      input.getStyleClass();
+   }
+
+ public String getValue(){
+    return input.getText();
+ }
+ public TextField getNode(){
+    return input;
+ }
 }
