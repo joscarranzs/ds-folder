fc72d61af9438edb795f55eb15f8419ba4a1a1f1
joscarranzs <jcarranzsosa@gmail.com>
2026-05-12 00:09:18 -0500

    revert: restore original behavior for side panels (remove overlay/collapsible experiment)

--- projects/Proyecto1_JoseCarranza/src/main/java/com/ds/application/view/Interface.java
+++ projects/Proyecto1_JoseCarranza/src/main/java/com/ds/application/view/Interface.java
@@ -20,7 +20,6 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 
